26a222e6b3e834ce8ceb532e9c4a7a9d673d3b77
Nic Mortelliti <npmortelliti@gmail.com>
2023-05-16 20:30:25 -0700

    restrict paintings routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :paintings
+  resources :paintings, only: [:index, :show, :new, :create, :destroy]
   resources :galleries
   resources :artists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
