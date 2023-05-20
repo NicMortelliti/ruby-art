a6f0db1acd25410fa433aef6dbe4978ea9dc0c28
Nic Mortelliti <npmortelliti@gmail.com>
2023-05-20 13:43:12 -0700

    add faker to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
+require 'faker'
+
+puts 'seeding data...'
+
 Artist.destroy_all
 Gallery.destroy_all
 Painting.destroy_all
 
-artist1 = Artist.create(name: "Leo", age: 100, experience: 1000)
-artist2 = Artist.create(name: "Vincent", age: 42, experience: 384)
-artist3 = Artist.create(name: "Mark", age: 77, experience: 56)
-artist4 = Artist.create(name: "Ralph", age: 150, experience: 433)
-artist5 = Artist.create(name: "Maria", age: 33, experience: 73)
-artist6 = Artist.create(name: "Rose", age: 10, experience: 950)
+artist1 = Artist.create(name: "Leo", age: 100, experience_level: 1000)
+artist2 = Artist.create(name: "Vincent", age: 42, experience_level: 384)
+artist3 = Artist.create(name: "Mark", age: 77, experience_level: 56)
+artist4 = Artist.create(name: "Ralph", age: 150, experience_level: 433)
+artist5 = Artist.create(name: "Maria", age: 33, experience_level: 73)
+artist6 = Artist.create(name: "Rose", age: 10, experience_level: 950)
 
 gal1 = Gallery.create(name: "gallery 1", location: "NY")
 gal2 = Gallery.create(name: "gallery 2", location: "LA")
@@ -19,3 +23,5 @@ gal6 = Gallery.create(name: "gallery 6", location: "FL")
 5.times do
   Painting.create(name: Faker::Name.name, price: Faker::Number.between(from: 10000, to: 100000), artist_id: Artist.ids.sample, gallery_id: Gallery.ids.sample)
 end
+
+puts 'Done!'
